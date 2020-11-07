39840b3dd6721f501b05fa3c1d3987cf25981679
Benoit MERLE <benoitmerle@fserv.fr>
2020-11-07 19:44:41 +0100

    remove old program (ruby)

--- src/lib/merge_recursive.rb
+++
@@ -1,9 +0,0 @@
-# Merge recursive
-
-def merge_recursively(a, b)
-    if a.is_a?(Hash)
-        a.merge(b) {|key, a_item, b_item| merge_recursively(a_item, b_item) }
-    else
-        b
-    end
-end
